d9640b029be87f4b3a583ed2304c8cf0cdc29a70
Dimas Lesmana <dimaslesmana99@gmail.com>
2021-11-15 01:12:22 +0700

    feat: add content in login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,80 @@
-import { IonPage } from '@ionic/react';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonRouterLink,
+  IonRow,
+  IonText
+} from '@ionic/react';
+import { mailOutline, lockClosedOutline } from 'ionicons/icons';
 
 const Login: React.FC = () => {
+  const handleLoginClick = () => {
+    console.log("Login button clicked!");
+  };
+
   return (
     <IonPage>
-      <h1>Login Page</h1>
+      <IonContent color="primary">
+        <IonGrid style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '100%'
+        }}>
+
+          <IonCard className="ion-text-center">
+            <IonCardHeader>
+              <h1 style={{ fontWeight: 'bold' }}>
+                <IonText color="primary">Masuk</IonText>
+              </h1>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <IonRow>
+                <IonCol>
+                  <IonList>
+                    <IonItem>
+                      <IonIcon icon={mailOutline} color="primary" slot="start" />
+                      <IonInput placeholder="Masukkan email" />
+                    </IonItem>
+                    <IonItem>
+                      <IonIcon icon={lockClosedOutline} color="primary" slot="start" />
+                      <IonInput placeholder="Masukkan kata sandi" />
+                    </IonItem>
+                  </IonList>
+                </IonCol>
+              </IonRow>
+
+              <IonRow>
+                <IonCol>
+                  <IonButton color="primary" expand="block" shape="round" onClick={handleLoginClick}>Masuk</IonButton>
+                </IonCol>
+              </IonRow>
+
+              <IonRow>
+                <IonCol>
+                  <IonLabel>
+                    Tidak memiliki akun?
+                    <IonRouterLink color="primary" routerLink="/register" rel="noopener noreferrer"> Daftar sekarang.</IonRouterLink>
+                  </IonLabel>
+                </IonCol>
+              </IonRow>
+            </IonCardContent>
+          </IonCard>
+
+        </IonGrid>
+      </IonContent>
     </IonPage>
   );
 };
