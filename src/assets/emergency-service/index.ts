f7f3a65a26513f13991726476a6d7c9450a47be1
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-14 15:03:33 +0700

    chore: add emergency service assets

---
+++ src/assets/emergency-service/index.ts
@@ -0,0 +1,8 @@
+export { default as ambulans } from './ambulans.png';
+export { default as basarnas } from './basarnas.png';
+export { default as bnpb } from './bnpb.png';
+export { default as callCenter } from './call-center.png';
+export { default as damkar } from './damkar.png';
+export { default as jasaMarga } from './jasa-marga.png';
+export { default as pln } from './pln.png';
+export { default as polisi } from './polisi.png';
