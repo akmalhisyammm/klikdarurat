3a492849060037191fc2bf615045acc858aacba0
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-16 11:13:16 +0700

    style: improve profile and edit profile layout

--- src/pages/main/profile/EditProfile.tsx
+++ src/pages/main/profile/EditProfile.tsx
@@ -1,14 +1,64 @@
-import { IonAvatar, IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import { closeOutline } from 'ionicons/icons';
+import {
+  IonAvatar,
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { folder } from 'ionicons/icons';
+import { useEffect, useState } from 'react';
+
+interface User {
+  name: string;
+  bio: string;
+  gender: string;
+  email: string;
+  phone: string;
+  address: string;
+}
 
 const EditProfile: React.FC = () => {
+  const [userData, setUserData] = useState<User>({
+    name: 'John Doe',
+    bio: 'Hello World',
+    gender: 'male',
+    email: 'john.doe@domain.com',
+    phone: '08123456789',
+    address: 'Jl. Mawar No. 1',
+  });
+
+  useEffect(() => {
+    setUserData({
+      name: 'John Doe',
+      bio: 'Hello World',
+      gender: 'male',
+      email: 'john.doe@domain.com',
+      phone: '08123456789',
+      address: 'Jl. Mawar No. 1',
+    });
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar color="danger">
           <IonTitle>Edit Profil</IonTitle>
           <IonButtons slot="start">
-            <IonBackButton defaultHref="/"/>
+            <IonBackButton defaultHref="/" />
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -17,65 +67,104 @@ const EditProfile: React.FC = () => {
         <IonGrid className="ion-text-center">
           <IonRow className="ion-margin-vertical">
             <IonCol>
-              <IonAvatar>
-                <img src="https://i.pravatar.cc/300"/>
-              </IonAvatar>              
-            </IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol>
-              <IonButton shape="round" color="secondary">PILIH FOTO</IonButton>
+              <IonAvatar
+                style={{
+                  width: '160px',
+                  height: '160px',
+                  margin: '4px auto',
+                  border: '2px solid var(--ion-color-danger)',
+                }}
+              >
+                <img src="https://i.pravatar.cc/300?img=13" alt="avatar" />
+              </IonAvatar>
+              <IonButton fill="clear" color="danger">
+                <IonIcon slot="start" icon={folder} />
+                <IonLabel>Pilih Foto</IonLabel>
+              </IonButton>
             </IonCol>
           </IonRow>
 
           <IonRow className="ion-margin-vertical">
             <IonCol>
               <IonList>
-              <IonItem>
-                  <IonLabel position="fixed">
+                <IonItem>
+                  <IonLabel position="fixed" color="primary">
                     Nama
                   </IonLabel>
-                  <IonInput placeholder="John Doe"/>
-                  <IonIcon icon={closeOutline} slot="end"/>
+                  <IonInput value={userData.name} inputMode="text" clearInput />
                 </IonItem>
 
                 <IonItem>
-                  <IonLabel position="fixed">
+                  <IonLabel position="fixed" color="primary">
+                    Bio
+                  </IonLabel>
+                  <IonInput value={userData.bio} inputMode="text" clearInput />
+                </IonItem>
+
+                <IonItem>
+                  <IonLabel position="fixed" color="primary">
+                    Gender
+                  </IonLabel>
+                  <IonSelect
+                    value={userData.gender}
+                    onIonChange={(e) => e.detail.value}
+                  >
+                    <IonSelectOption value="male">Laki-Laki</IonSelectOption>
+                    <IonSelectOption value="female">Perempuan</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+
+                <IonItem>
+                  <IonLabel position="fixed" color="primary">
                     E-mail
                   </IonLabel>
-                  <IonInput placeholder="john.doe@domain.com"/>
-                  <IonIcon icon={closeOutline} slot="end"/>
+                  <IonInput
+                    value={userData.email}
+                    inputMode="email"
+                    clearInput
+                  />
                 </IonItem>
 
                 <IonItem>
-                  <IonLabel position="fixed">
+                  <IonLabel position="fixed" color="primary">
                     No. Telp
                   </IonLabel>
-                  <IonInput placeholder="08123456789"/>
-                  <IonIcon icon={closeOutline} slot="end"/>
+                  <IonInput
+                    value={userData.phone}
+                    inputMode="tel"
+                    maxlength={12}
+                    clearInput
+                  />
                 </IonItem>
 
                 <IonItem>
-                  <IonLabel position="fixed">
+                  <IonLabel position="fixed" color="primary">
                     Alamat
                   </IonLabel>
-                  <IonInput placeholder="Jl. Mawar No. 1"/>
-                  <IonIcon icon={closeOutline} slot="end"/>
+                  <IonInput
+                    value={userData.address}
+                    inputMode="text"
+                    clearInput
+                  />
                 </IonItem>
-
               </IonList>
             </IonCol>
           </IonRow>
 
           <IonRow>
             <IonCol>
-              <IonButton shape="round" expand="block" color="primary">SIMPAN</IonButton>
+              <IonButton
+                shape="round"
+                expand="block"
+                color="primary"
+                routerLink="/main/profile"
+              >
+                Simpan
+              </IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
-
     </IonPage>
   );
 };
--- src/pages/main/profile/Profile.tsx
+++ src/pages/main/profile/Profile.tsx
@@ -1,11 +1,36 @@
-import { IonAvatar, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonAvatar,
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import {
+  callOutline,
+  logOut,
+  mailOutline,
+  maleOutline,
+  mapOutline,
+} from 'ionicons/icons';
 
 const Profile: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar color="danger">
-          <IonTitle slot="start">Profil Saya</IonTitle>
+          <IonTitle>Profil Saya</IonTitle>
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
@@ -13,13 +38,45 @@ const Profile: React.FC = () => {
       </IonHeader>
 
       <IonContent>
-        <IonGrid className="ion-text-center">
+        <IonGrid>
           <IonRow className="ion-margin-vertical">
             <IonCol>
-              <IonAvatar>
-                <img src="https://i.pravatar.cc/300"/>
-              </IonAvatar>
-              <h3 className="ion-text-center">John Doe</h3>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <IonAvatar
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    margin: '0 18px',
+                    border: '2px solid var(--ion-color-danger)',
+                  }}
+                >
+                  <img src="https://i.pravatar.cc/300?img=13" alt="avatar" />
+                </IonAvatar>
+                <IonText
+                  style={{
+                    borderLeft: '2px solid var(--ion-color-dark)',
+                    paddingLeft: '18px',
+                  }}
+                >
+                  <h3 style={{ fontWeight: 'bold' }}>
+                    <IonText color="danger">John Doe</IonText>
+                  </h3>
+                  <p>Hello World</p>
+                </IonText>
+              </div>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton
+                shape="round"
+                expand="block"
+                color="primary"
+                routerLink="/edit-profile"
+              >
+                Edit Profil
+              </IonButton>
             </IonCol>
           </IonRow>
 
@@ -27,36 +84,44 @@ const Profile: React.FC = () => {
             <IonCol>
               <IonList>
                 <IonItem>
-                  <IonLabel position="fixed">E-mail</IonLabel>
-                  <IonInput placeholder="john.doe@domain.com" disabled/>
+                  <IonIcon icon={maleOutline} slot="start" color="primary" />
+                  <IonLabel>Laki-Laki</IonLabel>
                 </IonItem>
 
                 <IonItem>
-                  <IonLabel position="fixed">
-                    No. Telp
-                  </IonLabel>
-                  <IonInput placeholder="08123456789" disabled/>
+                  <IonIcon icon={mailOutline} slot="start" color="primary" />
+                  <IonLabel>john.doe@domain.com</IonLabel>
                 </IonItem>
 
                 <IonItem>
-                  <IonLabel position="fixed">
-                    Alamat
-                  </IonLabel>
-                  <IonInput placeholder="Jl. Mawar No. 1" disabled/>
+                  <IonIcon icon={callOutline} slot="start" color="primary" />
+                  <IonLabel>08123456789</IonLabel>
                 </IonItem>
 
+                <IonItem>
+                  <IonIcon icon={mapOutline} slot="start" color="primary" />
+                  <IonLabel>Jl. Mawar No. 1</IonLabel>
+                </IonItem>
               </IonList>
             </IonCol>
           </IonRow>
 
-          <IonRow>
+          <IonRow className="ion-margin-vertical ion-text-center">
             <IonCol>
-              <IonButton shape="round">EDIT PROFIL</IonButton>
+              <IonButton
+                fill="outline"
+                shape="round"
+                expand="block"
+                color="danger"
+                routerLink="/"
+              >
+                <IonIcon icon={logOut} slot="start" />
+                <IonLabel>Keluar</IonLabel>
+              </IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
-
     </IonPage>
   );
 };
