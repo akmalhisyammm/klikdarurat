36e39e5a383c8987f87160da94fac6a07eea12c2
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-16 19:17:57 +0700

    style: improve personal service layout

--- src/pages/main/PersonalService.tsx
+++ src/pages/main/PersonalService.tsx
@@ -1,85 +1,102 @@
-import { IonContent,IonIcon, IonSearchbar,IonItemOptions,IonList, IonFab, IonFabButton,IonItemGroup,IonButton, IonItemSliding,IonItemDivider, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar,IonListHeader, IonItem, IonLabel, IonAvatar,IonThumbnail, IonImg, IonCard, IonNote } from '@ionic/react';
-import { add, swapVerticalOutline  } from 'ionicons/icons';
-import React,  { useState,useRef } from 'react';
-
-
+import {
+  IonButtons,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemDivider,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonNote,
+  IonPage,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { add, swapVertical } from 'ionicons/icons';
 
 const PersonalService: React.FC = () => {
-
-    return (
-      <IonPage>
-        <IonHeader  >
-         <h1> Personal Servis Page</h1>
-         <IonToolbar color="primary" >
-         <p> </p>
-        <IonSearchbar color="light" placeholder="Cari Kontak..."></IonSearchbar>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="danger">
+          <IonTitle>Kontak Darurat</IonTitle>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
         </IonToolbar>
-        </IonHeader>
-        <IonContent >
-      
-            <IonListHeader>  A -Z </IonListHeader>
-                    
-          <IonList>
-        <IonItemGroup>
-          <IonItemDivider>
-            <IonLabel>A</IonLabel>
-          </IonItemDivider>
+      </IonHeader>
+
+      <IonContent>
+        <IonToolbar color="primary">
+          <IonSearchbar
+            color="light"
+            placeholder="Cari Kontak..."
+            style={{
+              '--border-radius': '24px',
+              '--box-shadow': '0 0 0 1px var(--ion-color-dark)',
+              margin: '12px 0 8px',
+              padding: '0 6px',
+            }}
+          />
+        </IonToolbar>
+
+        <IonList>
           <IonItem>
-            <IonLabel color ="primary" >Ade Kiswara</IonLabel>
-            <IonNote slot="end"> Note </IonNote>
+            <IonLabel>A-Z</IonLabel>
+            <IonIcon icon={swapVertical} slot="end" />
           </IonItem>
-      
+          <IonItemGroup>
+            <IonItemDivider>
+              <IonLabel>A</IonLabel>
+            </IonItemDivider>
+            <IonItem>
+              <IonLabel>Ade Kiswara</IonLabel>
+              <IonNote slot="end"> Note </IonNote>
+            </IonItem>
 
-          <IonItemDivider>
-            <IonLabel>D</IonLabel>
-          </IonItemDivider>
+            <IonItemDivider>
+              <IonLabel>D</IonLabel>
+            </IonItemDivider>
+            <IonItem lines="inset">
+              <IonLabel>Dimas Lesmana</IonLabel>
+              <IonNote slot="end"> Note </IonNote>
+            </IonItem>
 
+            <IonItemDivider>
+              <IonLabel>I</IonLabel>
+            </IonItemDivider>
+            <IonItem lines="full">
+              <IonLabel>Indra Prasetya Hadiwana</IonLabel>
+              <IonNote slot="end"> Note </IonNote>
+            </IonItem>
 
-          <IonItem lines="inset">
-            <IonLabel color ="primary" >Dimas Lesmana</IonLabel>
-            <IonNote slot="end"> Note </IonNote>
-          </IonItem>
- 
-
-          <IonItemDivider>
-            <IonLabel>I</IonLabel>
-          </IonItemDivider>
-
-          <IonItem lines="full">
-            <IonLabel color ="primary" >Indra Prasetya Hadiwana</IonLabel>
-            <IonNote slot="end"> Note </IonNote>
-          </IonItem>
-
-
-
-          <IonItemDivider>
-            <IonLabel>M</IonLabel>
-          </IonItemDivider>
-
-          <IonItem >
-            <IonLabel color ="primary" >Muhammad Akmal Hiyam</IonLabel>
-            <IonNote slot="end"> Note </IonNote>
-          </IonItem>
-          <IonItem>
-            <IonLabel color ="primary" >Muhammad Rezalutfi</IonLabel>
-            <IonNote slot="end"> Note </IonNote>
-          </IonItem>
-
-
+            <IonItemDivider>
+              <IonLabel>M</IonLabel>
+            </IonItemDivider>
+            <IonItem>
+              <IonLabel>Muhammad Akmal Hiyam</IonLabel>
+              <IonNote slot="end"> Note </IonNote>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Muhammad Rezalutfi</IonLabel>
+              <IonNote slot="end"> Note </IonNote>
+            </IonItem>
           </IonItemGroup>
-            </IonList>
+        </IonList>
 
-            <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
           <IonFabButton>
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
-
-  
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default PersonalService;
