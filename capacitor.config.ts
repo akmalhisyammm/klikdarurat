28554549086b7d7afbb8ee198f2e6a52d56185b1
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-24 13:58:45 +0700

    chore: update capacitor version

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'id.ac.umn.ionic.klikdarurat',
+  appName: 'klikdarurat',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
