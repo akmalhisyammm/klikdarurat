037e2df865d1d934c3759a937ac8f5a9bc272f82
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-12-03 15:10:48 +0700

    chore: adjust appName in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'id.ac.umn.ionic.klikdarurat',
-  appName: 'klikdarurat',
+  appName: 'KlikDarurat',
   webDir: 'build',
   bundledWebRuntime: false,
 };
