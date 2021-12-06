387112a17ef479447d9263d5a28ddd312b55e964
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-12-06 23:24:35 +0700

    feat: add android project and setup app icon & splash screen

---
+++ android/app/src/main/java/id/ac/umn/ionic/klikdarurat/MainActivity.java
@@ -0,0 +1,61 @@
+package id.ac.umn.ionic.klikdarurat;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+    void setDarkMode() {
+        // Android "fix" for enabling dark mode
+        // @see: https://github.com/ionic-team/capacitor/discussions/1978
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        WebSettings webSettings = this.bridge.getWebView().getSettings();
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                // As of Android 10, you can simply force the dark mode
+                webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
+            }
+            // Before Android 10, we need to use a CSS class based fallback
+            this.bridge.getWebView().evaluateJavascript("document.body.classList.toggle('dark', true);", null);
+        } else {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                webSettings.setForceDark(WebSettings.FORCE_DARK_OFF);
+            }
+            this.bridge.getWebView().evaluateJavascript("document.body.classList.toggle('dark', false);", null);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setDarkMode();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setDarkMode();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initializes the Bridge
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            // Additional plugins you've installed go here
+            // Ex: add(TotallyAwesomePlugin.class);
+        }});
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setDarkMode();
+    }
+}
